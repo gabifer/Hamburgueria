3366244608246b53d98f804c9b795b981910d289
Gabriela Lima <gabifer@users.noreply.github.com>
2019-01-09 20:21:14 -0200

    Create PedidoControlerTest.java

---
+++ Hamburgueria/Projeto MVC/test/Controller/PedidoControlerTest.java
@@ -0,0 +1,24 @@
+package Controller;
+
+import Banco.ConnectionFactory;
+import Model.Pedido;
+import Controller.PedidoControler;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class PedidoControlerTest 
+{ 
+    public PedidoControlerTest() {}
+
+    @Test
+    /*public void testInserir() throws Exception 
+    {
+        PedidoControler pedido = new PedidoControler();
+        
+        if(pedido.Pedir(pedido).save)
+            fail("Erro ao salvar pedido");
+        
+        else
+            System.out.println("Pedido salvo com sucesso");
+    }*/
+}
